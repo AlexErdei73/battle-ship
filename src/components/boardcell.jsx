4e65578fac538d7233c0177dfdc861685f6d94e1
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-01-30 02:05:01 +0000

    add icons and styling to boardCell

--- src/components/boardcell.jsx
+++ src/components/boardcell.jsx
@@ -1,4 +1,35 @@
+import battleShip from "../icons/battleship.svg";
+import hit from "../icons/hit.png";
+import sunk from "../icons/sunk.jpg";
+import missed from "../icons/missed.png";
+
 const BoardCell = (props) => {
+  let picture;
+  const style = {};
+
+  switch (props.content[props.pos]) {
+    case "B":
+      picture = battleShip;
+      style.backgroundColor = "skyblue";
+      style.borderColor = "steelblue";
+      break;
+    case "S":
+      picture = sunk;
+      style.backgroundColor = "whitesmoke";
+      style.borderColor = "yellow";
+      break;
+    case "X":
+      picture = hit;
+      style.backgroundColor = "yellow";
+      style.borderColor = "red";
+      break;
+    case "/":
+      picture = missed;
+      break;
+    default:
+      picture = "#";
+  }
+
   return (
     <button
       className="boardCell"
@@ -6,8 +37,9 @@ const BoardCell = (props) => {
       key={props.pos}
       onClick={props.onClick}
       disabled={props.disabled}
+      style={style}
     >
-      {props.content[props.pos]}
+      <img alt="" src={picture} width="20px" />
     </button>
   );
 };
