47b5bdc8739a9d6f6b8526e4f4a12429d50da591
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-02-11 16:03:40 +0000

    make computer play smart

--- src/game.js
+++ src/game.js
@@ -37,7 +37,7 @@ function Game() {
       if (gameOver) winner = this.player;
     } else {
       do {
-        attackResult = this.computer.autoAttack(randomPosition);
+        attackResult = this.computer.autoAttackSmart(randomPosition);
         //update the DOM here
         this.setState(getState(this));
         gameOver = this.player.board.isAllShipSunk();
